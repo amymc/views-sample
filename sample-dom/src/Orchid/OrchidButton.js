27ef4189a4c2627307a4b7af75f02eda0aea204f
Darío Javier Cravero <dario@uxtemple.com>
2018-02-13 23:32:47 +0000

    chore: remove unused React from orchid that caused a warning

--- sample-dom/src/Orchid/OrchidButton.js
+++ sample-dom/src/Orchid/OrchidButton.js
@@ -1,6 +1,5 @@
 // @view
 import 'orchid/web/library/static/css/orchid.css'
 import * as Orchid from 'orchid/web/library/orchid-library.min.js'
-import React from 'react'
 
 export default Orchid.Button
